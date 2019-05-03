25c78772e29403887345535a42d5b28bab167064
mrmelchi <Mario.Melchiori@gmail.com>
2019-05-03 20:40:19 -0300

    New commit movielens capstone Harvard

--- Capstone_Movielens.R
+++ Capstone_Movielens.R
@@ -1,11 +1,11 @@
-## ------------------------------------------------------------------------
+#############################################################
+#---------------------- function ---------------------------
+#############################################################
 ## RMSE compute root mean square error (RMSE)
 RMSE <- function(true_ratings, predicted_ratings){
     sqrt(mean((true_ratings - predicted_ratings)^2))
 }
 
-
-## ------------------------------------------------------------------------
 #############################################################
 # Create edx set, validation set, and submission file
 #############################################################
@@ -53,8 +53,6 @@ edx <- rbind(edx, removed)
 
 rm(dl, ratings, movies, test_index, temp, movielens, removed)
 
-
-## ------------------------------------------------------------------------
 # generate reproducible partition
 
 set.seed(1)
@@ -76,25 +74,20 @@ test <- temp %>%
 removed <- anti_join(temp, test)
 train <- rbind(train, removed)
 
-
-## ------------------------------------------------------------------------
 # to save space
 rm(removed, temp, test_index) 
 
-
-## ------------------------------------------------------------------------
+# show train set 
 train %>% as_tibble()
 
-
-## ------------------------------------------------------------------------
 train %>% summarize(
-  n_users=n_distinct(userId),
-  n_movies=n_distinct(movieId),
-  min_rating=min(rating),  
-  max_rating=max(rating))
+n_users=n_distinct(userId),# unique users from train
+n_movies=n_distinct(movieId),# unique movies from train
+min_rating=min(rating),  # the lowest rating 
+max_rating=max(rating) # the highest rating
+)
 
-
-## ------------------------------------------------------------------------
+# matrix for 5 movies and 7 users 
 keep <- train %>% 
   count(movieId) %>% 
   top_n(5, n) %>% 
@@ -109,8 +102,9 @@ tab <- train %>%
   spread(title, rating)
 tab %>% knitr::kable()
 
+# matrix for a random sample of 100 movies and 100 users with yellow 
+# indicating a user/movie combination for which we have a rating.
 
-## ------------------------------------------------------------------------
 users <- sample(unique(train$userId), 100)
 rafalib::mypar()
 train %>% filter(userId %in% users) %>% 
@@ -121,101 +115,84 @@ train %>% filter(userId %in% users) %>%
   image(1:100, 1:100,. , xlab="Movies", ylab="Users")
 abline(h=0:100+0.5, v=0:100+0.5, col = "grey")
 
-
-## ------------------------------------------------------------------------
+# plot count rating by movie
 train %>% 
   count(movieId) %>% 
   ggplot(aes(n)) + 
-  geom_histogram(bins = 30, color = "black") + 
+  geom_histogram(bins = 30, color = "blue") + 
   scale_x_log10() + 
   ggtitle("Movies")
 
-
-## ------------------------------------------------------------------------
+# plot count rating by user
 train %>% 
   count(userId) %>% 
   ggplot(aes(n)) + 
-  geom_histogram(bins = 30, color = "black") + 
+  geom_histogram(bins = 30, color = "orange") + 
   scale_x_log10() + 
   ggtitle("Users")
 
-
-## ------------------------------------------------------------------------
 # to save space
 rm(tab, keep, users)
 
-
-## ------------------------------------------------------------------------
 mu <- mean(train$rating) # compute mean rating
 mu
 
 naive_rmse <- RMSE(test$rating, mu) # compute root mean standard error
 naive_rmse
 
+# create a results table with this approach
 
-## ------------------------------------------------------------------------
 rmse_results <- tibble(method = "Just the average", RMSE = naive_rmse)
 rmse_results %>% knitr::kable()
 
-
-## ---- eval=FALSE---------------------------------------------------------
 ## fit <- lm(rating ~ as.factor(movieId), data = train)
 
-
-## ------------------------------------------------------------------------
 mu <- mean(train$rating) 
 movie_avgs <- train %>% 
   group_by(movieId) %>% 
   summarize(b_i = mean(rating - mu))
 movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
 
-
-## ------------------------------------------------------------------------
+# create a results table with this and prior approaches
 predicted_ratings <- mu + test %>% 
   left_join(movie_avgs, by='movieId') %>%
   .$b_i
 
+# create a results table with this and prior approach
 model_1_rmse <- RMSE(predicted_ratings, test$rating)
 rmse_results <- bind_rows(rmse_results,
                           tibble(method="Movie Effect Model on test set",  
                                      RMSE = model_1_rmse ))
 rmse_results %>% knitr::kable()
 
-
-## ------------------------------------------------------------------------
 train %>% 
   group_by(userId) %>% 
   summarize(b_u = mean(rating - mu)) %>% 
   ggplot(aes(b_u)) + 
   geom_histogram(bins = 30, color = "blue")
 
-
-## ---- eval = FALSE-------------------------------------------------------
 ## lm(rating ~ as.factor(movieId) + as.factor(userId))
 
-
-## ------------------------------------------------------------------------
+# compute user effect b_u
 user_avgs <- train %>% 
   left_join(movie_avgs, by='movieId') %>%
   group_by(userId) %>%
   summarize(b_u = mean(rating - mu - b_i))
 
-
-## ------------------------------------------------------------------------
+# compute predicted values on test set
 predicted_ratings <- test %>% 
   left_join(movie_avgs, by='movieId') %>%
   left_join(user_avgs, by='userId') %>%
   mutate(pred = mu + b_i + b_u) %>%
   .$pred
 
+# create a results table with this and prior approaches
 model_2_rmse <- RMSE(predicted_ratings, test$rating)
 rmse_results <- bind_rows(rmse_results,
                           tibble(method="Movie and User Effects Model on test set",  
                                      RMSE = model_2_rmse ))
 rmse_results %>% knitr::kable()
 
-
-## ------------------------------------------------------------------------
 # Regularization
 
 # connect movieId to movie title
@@ -223,8 +200,6 @@ movie_titles <- train %>%
   select(movieId, title) %>%
   distinct()
 
-
-## ------------------------------------------------------------------------
 # top 10 best movies based on b_i
 movie_avgs %>% left_join(movie_titles, by="movieId") %>%
   arrange(desc(b_i)) %>% 
@@ -232,8 +207,6 @@ movie_avgs %>% left_join(movie_titles, by="movieId") %>%
   slice(1:10) %>%  
   knitr::kable()
 
-
-## ------------------------------------------------------------------------
 # top 10 worse movies based on b_i
 movie_avgs %>% left_join(movie_titles, by="movieId") %>%
   arrange(b_i) %>% 
@@ -241,8 +214,6 @@ movie_avgs %>% left_join(movie_titles, by="movieId") %>%
   slice(1:10) %>%  
   knitr::kable()
 
-
-## ------------------------------------------------------------------------
 # add number of rating of the "best" obscure movies
 train %>% count(movieId) %>% 
   left_join(movie_avgs) %>%
@@ -252,8 +223,6 @@ train %>% count(movieId) %>%
   slice(1:10) %>% 
   knitr::kable()
 
-
-## ------------------------------------------------------------------------
 # add number of rating of the "worse" obscure movies
 
 train %>% count(movieId) %>% 
@@ -264,8 +233,8 @@ train %>% count(movieId) %>%
   slice(1:10) %>% 
   knitr::kable()
 
-
-## ------------------------------------------------------------------------
+# use cross-validation to pick a lambda:
+  
 lambda <- seq(0, 10, 0.25)
 
 rmses <- sapply(lambda, function(l){
@@ -291,27 +260,31 @@ rmses <- sapply(lambda, function(l){
 })
 
 qplot(lambda, rmses)  
-
+# pick lambda with minimun rmse
 lambda <- lambda[which.min(rmses)]
+# print lambda
+lambda
 
-
-## ------------------------------------------------------------------------
-b_i <- test %>% 
+# compute movie effect with regularization on train set
+b_i <- train %>% 
   group_by(movieId) %>%
   summarize(b_i = sum(rating - mu)/(n()+lambda))
 
-b_u <- test %>% 
+# compute user effect with regularization on train set
+b_u <- train %>% 
     left_join(b_i, by="movieId") %>%
     group_by(userId) %>%
     summarize(b_u = sum(rating - b_i - mu)/(n()+lambda))
-  
+
+# # compute predicted values on test set 
 predicted_ratings <- 
     test %>% 
     left_join(b_i, by = "movieId") %>%
     left_join(b_u, by = "userId") %>%
     mutate(pred = mu + b_i + b_u) %>%
     pull(pred)
- 
+
+# create a results table with this and prior approaches
 model_3_rmse <- RMSE(test$rating, predicted_ratings)
 
 rmse_results <- bind_rows(rmse_results,
@@ -319,34 +292,47 @@ rmse_results <- bind_rows(rmse_results,
                                  RMSE = model_3_rmse))
 rmse_results %>% knitr::kable()
 
+# compute movie effect without regularization on train set
+b_i <- train %>% 
+  group_by(movieId) %>%
+  summarize(b_i = mean(rating - mu))
 
-## ------------------------------------------------------------------------
+# compute user effect without regularization on train set
+b_u <- train %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>%
+    summarize(b_u = mean(rating - b_i - mu))
+
+# compute residuals on train set
 train <- train %>% 
   left_join(b_i, by = "movieId") %>%
   left_join(b_u, by = "userId") %>%
   mutate(res = rating - mu - b_i - b_u)
 
+# compute residuals on test set
 test <- test %>% 
   left_join(b_i, by = "movieId") %>%
   left_join(b_u, by = "userId") %>%
   mutate(res = rating - mu - b_i - b_u)
 
 
-## ------------------------------------------------------------------------
-
-# Using recosystem 
 # Install/Load recosystem
 if(!require(recosystem)) install.packages("recosystem", repos = "http://cran.us.r-project.org")
 
+# create data saved on disk in 3 columns with no headers
 train_data <- data_memory(user_index = train$userId, item_index = train$movieId, 
                           rating = train$res, index1 = T)
   
 test_data <- data_memory(user_index = test$userId, item_index = test$movieId, index1 = T)
   
+# create a model object
 recommender <- Reco()
 
-## ------------------------------------------------------------------------
-set.seed(1) # This is a randomized algorithm
+# This is a randomized algorithm
+set.seed(1) 
+
+# call the `$tune()` method to select best tuning parameters.
+# We comment these lines because executing this code takes about one hour 
 
 # res = recommender$tune(
 #     train_data,
@@ -354,7 +340,10 @@ set.seed(1) # This is a randomized algorithm
 #                 costp_l1 = 0, costq_l1 = 0,
 #                 lrate = c(0.05, 0.1, 0.2), nthread = 2)
 # )
-#  res[["min"]]
+
+# this is the output
+
+# res$min
 # $dim
 # [1] 30
 # 
@@ -371,91 +360,104 @@ set.seed(1) # This is a randomized algorithm
 # [1] 0.1
 # 
 # $lrate
-# [1] 0.1
+# [1] 0.05
 # 
 # $loss_fun
-# [1] 0.8007686
+# [1] 0.7983297
 
-
-
-## ------------------------------------------------------------------------
-# Training model from a data file
-set.seed(1) # This is a randomized algorithm
+# Train the model by calling the `$train()` method
+# some parameters coming from the result of `$tune()`
+# This is a randomized algorithm
+set.seed(1) 
 suppressWarnings(recommender$train(train_data, opts = c(dim = 30, costp_l1 = 0,
                                                         costp_l2 = 0.01, costq_l1 = 0,
-                                                        costq_l2 = 0.1, lrate = 0.1,
+                                                        costq_l2 = 0.1, lrate = 0.05,
                                                         verbose = FALSE)))
 
-
-## ------------------------------------------------------------------------
+# use the `$predict()` method to compute predicted values
+# return predicted values in memory
 predicted_ratings <- recommender$predict(test_data, out_memory()) + mu + test$b_i + test$b_u 
 
-
-## ------------------------------------------------------------------------
+# ceiling rating at 5
 ind <- which(predicted_ratings > 5)
 predicted_ratings[ind] <- 5
-  
+
+# floor rating at 0.50  
 ind <- which(predicted_ratings < 0.5)
 predicted_ratings[ind] <- 0.5
   
+# create a results table with this approach
 model_4_rmse <- RMSE(test$rating, predicted_ratings)
   
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Reg. Movie and User + Matrix Fact. on test set",  
+                          tibble(method="Movie and User + Matrix Fact. on test set",  
                                    RMSE = model_4_rmse))
 rmse_results %>% knitr::kable()
 
 
-
-## ------------------------------------------------------------------------
-
-b_i <- edx %>% 
+# compute movies effect without regularization on edx set
+b_i <- edx %>%
   group_by(movieId) %>%
-  summarize(b_i = sum(rating - mu)/(n()+lambda))
+  summarize(b_i = mean(rating - mu))
 
-b_u <- edx %>% 
+# compute users effect without regularization on edx set
+b_u <- edx %>%
     left_join(b_i, by="movieId") %>%
     group_by(userId) %>%
-    summarize(b_u = sum(rating - b_i - mu)/(n()+lambda))
+    summarize(b_u = mean(rating - b_i - mu))
 
+# compute residuals on edx set
 edx <- edx %>% 
   left_join(b_i, by = "movieId") %>%
   left_join(b_u, by = "userId") %>%
   mutate(res = rating - mu - b_i - b_u)
 
+# compute residuals on validation set
 validation <- validation %>% 
   left_join(b_i, by = "movieId") %>%
   left_join(b_u, by = "userId") %>%
   mutate(res = rating - mu - b_i - b_u)
 
 # Using recosystem 
-train_data <- data_memory(user_index = edx$userId, item_index = edx$movieId, 
+# create data saved on disk in 3 columns with no headers
+
+edx_data <- data_memory(user_index = edx$userId, item_index = edx$movieId, 
                           rating = edx$res, index1 = T)
   
-test_data <- data_memory(user_index = validation$userId, item_index = validation$movieId, index1 = T)
-  
+validation_data <- data_memory(user_index = validation$userId, item_index = validation$movieId, index1 = T)
+
+# create a model object  
 recommender <- Reco()
+
+# Train the model by calling the `$train()` method
+# some parameters coming from the result of `$tune()`
+# This is a randomized algorithm
 set.seed(1)
 
-suppressWarnings(recommender$train(train_data, opts = c(dim = 30, costp_l1 = 0,
+suppressWarnings(recommender$train(edx_data, opts = c(dim = 30, costp_l1 = 0,
                                                         costp_l2 = 0.01, costq_l1 = 0,
-                                                        costq_l2 = 0.1, lrate = 0.1,
+                                                        costq_l2 = 0.1, lrate = 0.05,
                                                         verbose = FALSE)))
 
+# use the `$predict()` method to compute predicted values
+# return predicted values in memory
 
-predicted_ratings <- recommender$predict(test_data, out_memory()) + mu + 
+predicted_ratings <- recommender$predict(validation_data, out_memory()) + mu + 
                        validation$b_i + validation$b_u 
 
-  
+# ceiling rating at 5  
 ind <- which(predicted_ratings > 5)
 predicted_ratings[ind] <- 5
-  
+
+# floor rating at 5  
 ind <- which(predicted_ratings < 0.5)
 predicted_ratings[ind] <- 0.5
   
+# create a results table with this and prior approaches
 model_5_rmse <- RMSE(validation$rating, predicted_ratings)
   
 rmse_results <- bind_rows(rmse_results,
-                          tibble(method="Reg. Movie and User + Matrix Fact. on validation set",  
+                          tibble(method="Movie and User effects and Matrix Fact. on validation set",  
                                    RMSE = model_5_rmse))
 rmse_results %>% knitr::kable()
+
